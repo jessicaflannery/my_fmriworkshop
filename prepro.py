8f514cb6d04b9e7c5781aff2680c588db8418ffc
Jessica Flannery <jflanner@uoregon.edu>
2017-12-12 08:13:46 -0800

    new prepro

--- prepro.py
+++ prepro.py
@@ -6,3 +6,21 @@ Created on Tue Dec 12 07:54:17 2017
 @author: jessicaflannery
 """
 
+
+import glob
+import os
+import pdb
+import subprocess
+import argparse
+import datetime
+import shutil
+
+def prepro(basedir):
+    print('Hello data in the path' +basedir)
+    
+def main():
+    basedir='/Users/jessicaflannery/Desktop/fMRI reporducibility workshop/data/'
+    prepro(basedir)
+    
+    
+main()
